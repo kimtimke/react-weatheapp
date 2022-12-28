a7c2411d56d5a3c3ba78314192f653ca329554c7
Namgirl <seebachkim@gmail.com>
2022-12-28 16:56:31 +0100

    added Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by Kim Timke and is
+        <a href="https://github.com/kimtimke/react-weatheapp" target="_blank">
+          {" "}
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
